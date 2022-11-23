a30afecc5f6bf66a88349febcda0be3bad41ea9a
My8698 <112363755+My8698@users.noreply.github.com>
2022-11-23 12:36:44 +0530

    program to find number is pallindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -1,25 +1,22 @@
 package com.bridgelabz.basics;
-
 import java.util.Scanner;
-
 public class Palindrome {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         System.out.println("Enter Number:");
-        int num= scn.nextInt();
+        int number= scn.nextInt();
         int reverse=0,rem,temp;
-        temp=num;
-
+        temp=number;
         while (temp!=0)//for finding reverse number
         {
             rem=temp%10;
             reverse=reverse*10+rem;
             temp/=10;
         };
-        if(num==reverse)
-            System.out.println(num + " is Palindrome");
+        if(number==reverse)
+            System.out.println(number + " is Palindrome");
         else
-            System.out.println(num + " is not Palindrome");
+            System.out.println(number + " is not Palindrome");
     }
 
 }
