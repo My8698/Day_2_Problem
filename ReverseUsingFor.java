d5348ca84b966443845299ee8c851884fc57a9c9
My8698 <112363755+My8698@users.noreply.github.com>
2022-11-23 12:39:18 +0530

    program to find reverse of given number

--- ReverseUsingFor.java
+++ ReverseUsingFor.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.basics;
-
 public class ReverseUsingFor {
     public static void main(String[] args) {
-        int num=123,reverse=0;
-        for (;num!=0;num=num/10)
+        int number=123,reverse=0;
+        for (;number!=0;number=number/10)
         {
-            int remainder=num%10;
+            int remainder=number%10;
             reverse=reverse*10+remainder;
         }
         System.out.println("The Reverse Number is->"+reverse);
