7c9e0a2eb6e75b8a7165a4a64480e311194ff461
My8698 <112363755+My8698@users.noreply.github.com>
2022-11-23 12:47:16 +0530

    Added:calculate sum of command line arguments

--- SumOfCommandLineArgs.java
+++ SumOfCommandLineArgs.java
@@ -1,10 +1,8 @@
 package com.bridgelabz.basics;
-
 public class SumOfCommandLineArgs {
     public static void main(String[] args) {
         int a, b, c;
         a = Integer.parseInt(args[0]);
-
         b = Integer.parseInt(args[1]);
         c=a+b;
         System.out.println("Sum->"+c);
